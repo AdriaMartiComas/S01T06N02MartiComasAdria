23bbd44e188267bfe8a208d16399ae3a301db106
adriamarticomas <adriamarticomas@192.168.1.129>
2022-07-22 12:08:59 +0200

    printGenericMethods metode de variable indefinida

--- S01T06N02/src/exercici2/App.java
+++ S01T06N02/src/exercici2/App.java
@@ -1,26 +1,22 @@
 package exercici2;
 
-import java.util.ArrayList;
-import java.util.List;
+
 
 public class App {
 
 	public static void main(String[] args) {
 		Persona persona1 = new Persona("Adrià", "Martí", 30);
-		String str = "Soc un string";
-		int num = 7;
-		
-		List<Object> myList = new ArrayList<Object>();
-		myList.add(persona1);
-		myList.add(str);
-		myList.add(num);
-		
-		GenericMethods.printGenerics(myList);
+		Persona persona2 = new Persona("Maria", "Rull", 33);
+		Persona persona3 = new Persona("Felipe", "Santos", 37);
+		Persona persona4 = new Persona("Nil", "Gasol", 25);
 
 
-		// NO FUNCIONA PERQUE EL PRIMER PARAMETRE HA DE SER DE TIPUS PERSONA
-		//System.out.println(new GenericMethods("Soc un String", persona1, 32));
-		//System.out.println(new GenericMethods(32, "souc un string", persona1));
+		System.out.println("2 persones");
+		GenericMethods.printGenericMethods(persona1, persona3);
+		System.out.println("4 persones");
+		GenericMethods.printGenericMethods(persona1, persona2, persona3, persona4);
+
+
 	}
 	
 	
--- S01T06N02/src/exercici2/GenericMethods.java
+++ S01T06N02/src/exercici2/GenericMethods.java
@@ -1,34 +1,25 @@
 package exercici2;
 
-import java.util.List;
-
-public class GenericMethods<T extends Persona, S, V> {
+public class GenericMethods<T, S, V> {
 	Persona ob1;
 	S ob2;
 	V ob3;
-	
+
 	public GenericMethods(Persona ob1, S ob2, V ob3) {
 		this.ob1 = ob1;
 		this.ob2 = ob2;
 		this.ob3 = ob3;
 	}
-	
-	public static void printGenerics(List<?> myList) {
-		for(Object obj: myList) {
-			System.out.println(obj);
+
+	public static void printGenericMethods(Persona... persones) {
+		for (Persona obj : persones) {
+			System.out.println(obj + " ");
 		}
 	}
-	
-	
 
 	@Override
 	public String toString() {
 		return "GenericMethods [ob1=" + ob1 + ", ob2=" + ob2 + ", ob3=" + ob3 + "]";
 	}
-	
-	
-	
-	
 
 }
-
